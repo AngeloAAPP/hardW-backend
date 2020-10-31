a5cc7dad2bf2a856fa25626baa83a30213dd3c40
angelo <angelo_aapp@hotmail.com>
2020-10-31 13:54:40 -0300

    configurando conexão bd heroku

--- src/config/database.js
+++ src/config/database.js
@@ -2,13 +2,29 @@ try{
     require('dotenv').config()
 }catch(err){}
 
+const stringDB = process.env.DATABASE_URL.substring(11)
+
+//Separa a string de conexão com o banco de dados em suas respectivas variaveis
+//Não foi utilizada a string diretamente para conseguir utilizar a linha de comando do sequelize
+
+const [username, passwordAndHost, portAndDatabase] = stringDB.split(":")
+const [password, host] = passwordAndHost.split("@")
+const [port, database] = portAndDatabase.split('/')
+
 module.exports = {
-    dialect: 'postgres',
-    database: 'hardw',
-    host: 'localhost',
-    username: process.env.DB_USER,
-    password: process.env.DB_PASS,
+    dialect: "postgres",
+    host,
+    port,
+    username,
+    password,
+    database,
+    dialectOptions:{
+        ssl: {
+            require: true,
+            rejectUnauthorized: false
+        }
+    },    
     define: {
-        timestamps: true
+        timestamps:true
     }
 }
